dce76734d2ca775688f2af7fb6d2edc5c75cd7fa
Manjula3101 <manjuradharapu@gmail.com>
2026-05-01 15:44:28 +0530

    Auto-save update for code mastery_Python_Variables and Data Types_71.py

---
+++ code mastery_Python_Variables and Data Types_71.py
@@ -0,0 +1,12 @@
+# Given variable
+x = 5.0
+
+# Check type
+if type(x) == int:
+    print("Integer")
+elif type(x) == float:
+    print("Float")
+elif type(x) == str:
+    print("String")
+else:
+    print("Other")
